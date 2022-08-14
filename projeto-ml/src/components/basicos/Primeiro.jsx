1c8b50f1a0fea48288b17556638448982e373d81
Catskhi <catskhi.studios@gmail.com>
2022-08-14 16:38:40 -0300

    first commit

---
+++ projeto-ml/src/components/basicos/Primeiro.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+// Código do comentário abaixo porém em Arrow Function
+export default () => 
+        <>
+            <h1>Primeiro Componente (Arrow)</h1>
+            <h2>Segundo Componente</h2>
+        </>
+
+
+// function Primeiro() {
+//     return (
+//     <div>
+//         <h1>Primeiro Componente</h1>
+//         <h2>Exemplo de um componente React</h2>
+//     </div>
+//     )
+// }
+
+// export default Primeiro
---
+++ projeto-ml/src/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+import React from 'react'
+import reactDOM from 'react-dom'
+
+import App from './App.jsx'
+
+reactDOM.render (
+    <App></App>,
+    document.getElementById('root')
+)
